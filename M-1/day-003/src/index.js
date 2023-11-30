dcb4b0a34b846fcc1748fdfcf924ae81ce4c2257
rxstd <xcoding.protonmail.com>
2023-11-30 14:25:21 +0900

    fix: day003 index.js 내용추가

--- M-1/day-003/src/index.js
+++ M-1/day-003/src/index.js
@@ -1,3 +1,7 @@
+//설치한 오픈소스 노드 패키지를 참조한다.
+//node.js에서는 require('설치패키지명') 예약어를 통해 지정한 설치된 노드패키지를 참조합니다.
+const moment = require("mement");
+
 console.log("최초 생성된 NodeJS Backend Module 파일입니다.");
 console.log("로그가 잘 출력됩니다.");
 console.log("저도 배가 고파요.");
