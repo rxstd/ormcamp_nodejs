e7928e2105061808d8f0f224b8e5de616c53e1fb
rxstd <xcoding@protonmail.com>
2023-12-07 13:33:24 +0900

    feat: day008 last update

--- M-1/day-008/src/sync-async-callback.js
+++ M-1/day-008/src/sync-async-callback.js
@@ -57,9 +57,16 @@ function aafn3(cb) {
 }
 
 // aafn1(() => {
+//   // 이렇게 하면 순서를 보장받을 수 있다.
 //   aafn2(() => {
+//     // 다만 지금 코드처럼 객체형식으로 함수를 넘기는 식으로 작업하면 코드가 복잡해진다.
 //     aafn3(() => {
-//       console.log("프로세스 종료");
+//       // 이런 것을 콜백지옥 (callback hell) 이라고 한다.
+//       aafn2(() => {
+//         aafn3(() => {
+//           console.log("end");
+//         });
+//       });
 //     });
 //   });
 // });
@@ -80,5 +87,23 @@ function aaafn3() {
   console.log("aaafn3");
 }
 
-aaafn1();
-aaafn2();
+// aaafn1();
+// aaafn2();
+
+function bfn1() {
+  console.log("bfn1");
+}
+
+function bfn2(cb) {
+  setTimeout(() => {
+    console.log("bfn2");
+    cb();
+  }, 2000);
+}
+
+function bfn3() {
+  console.log("bfn3");
+}
+
+bfn1();
+bfn2(bfn3);
